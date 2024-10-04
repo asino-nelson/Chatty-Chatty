18ebab56544f5bb6b0d66b649139fe4650b4f060
asino-nelson <asinonelson276@gmail.com>
2024-10-04 16:10:46 +0300

    cors debug

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -166,4 +166,20 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-csrf-token",
+]
+
+
 AUTH_USER_MODEL = 'api.User'
