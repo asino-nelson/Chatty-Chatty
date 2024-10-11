492af9fc3e45182e08693da0eabdc4739fc9505e
asino-nelson <asinonelson276@gmail.com>
2024-10-11 17:38:41 +0300

    cors debug

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -31,8 +31,8 @@ export const AuthProvider = ({ children }) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
+          // "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
         },
         body: JSON.stringify({
           email,
@@ -65,8 +65,8 @@ export const AuthProvider = ({ children }) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
+          // "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
         },
         body: JSON.stringify({
           email,
